41db6d7de272bca7b2b18184ecf3822cbb5c5f88
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2013-12-10 00:06:04 -0500

    packing extension

--- pack-extension.sh
+++ pack-extension.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-rm packed-extension.zip
+rm color-pusher-chrome-packed-extension.zip
 
-mkdir packed-extension
+mkdir color-pusher-chrome-packed-extension
 
-cp -r native panel icons background.js \
-      devtools.html devtools.js manifest.json \
-      packed-extension
+cp -r background.js content-script \
+      devtools.html devtools.js icons \
+      manifest.json panel \
+      color-pusher-chrome-packed-extension
 
-zip -r packed-extension.zip packed-extension
+zip -r color-pusher-chrome-packed-extension.zip color-pusher-chrome-packed-extension
 
 #rm -r packed-extension
