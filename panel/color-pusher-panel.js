ac9cd477b7c17426b19ec1787a696acea4d8cfdd
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2013-12-09 23:32:42 -0500

    using latest version of color pusher widget

--- panel/color-pusher-panel.js
+++ panel/color-pusher-panel.js
@@ -4,20 +4,13 @@
   panel.controller('color-pusher-ctrl', ['$scope', colorPusherCtrl]);
 
   function colorPusherCtrl($scope) {
+    console.log('colorPusherCtrl');
+
     $scope.$on('apply-colors', function onApplyColor(event, colors) {
-      console.log('colors', colors);
       check.verify.object(colors, 'expected colors to be an object ' +
         JSON.stringify(colors, null, 2));
 
-      // chrome.extension.sendMessage('apply-colors', colors);
       window.respond('apply-colors', colors);
-
-      /*
-      Object.keys(colors).forEach(function (selector) {
-        var css = colors[selector];
-        $(selector).css(css);
-      });
-      */
     });
   }
 }(angular));
